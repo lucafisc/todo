314e03377649cfd4272198f7067498b457d0ec0e
lucafisc <lucafischer@fastmail.com>
2022-07-04 17:35:55 +0200

    Add basic interface

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles/main.css";
+import "./styles/list-items.css";
+import "./styles/form.css";
+
+
+import "@fortawesome/fontawesome-free/js/all.js";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 
 
@@ -7,15 +13,17 @@ const colorControl = (() => {
     let root = document.documentElement;
     let mainColorHSL = getComputedStyle(root).getPropertyValue("--main-color").match(/(\d+)/gm);
     mainColorHSL = mainColorHSL.map(Number);
-    let secondaryColorHSL = "hsl(" + (mainColorHSL[0] + 1) + "," + (mainColorHSL[1] + 1) + "%," + ( mainColorHSL[2] - 5) + "%)";
-    let oppositeColorHSL = "hsl(" + (mainColorHSL[0] + 180) + "," + (mainColorHSL[1] + 10) + "%," + ( mainColorHSL[2] + 10) + "%)";
-    console.log(secondaryColorHSL);
+    let secondaryColorHSL = "hsl(" + (mainColorHSL[0] - 10) + "deg," + (mainColorHSL[1] - 20) + "%," + ( mainColorHSL[2] + 25) + "%)";
+    let iconColor = "hsl(" + (mainColorHSL[0] - 15) + "deg," + (mainColorHSL[1]) + "%," + ( mainColorHSL[2]) + "%)";
+    let flagColor = "hsl(" + (mainColorHSL[0] - 30) + "deg," + (mainColorHSL[1] + 20) + "%," + ( mainColorHSL[2]) + "%)";
+    let oppositeColorHSL = "hsl(" + (mainColorHSL[0] + 180) + "deg," + (mainColorHSL[1] + 10) + "%," + ( mainColorHSL[2] + 10) + "%)";
+    let oppositeColorHSLLighter = "hsl(" + (mainColorHSL[0] + 180) + "deg," + (mainColorHSL[1] + 10) + "%," + ( mainColorHSL[2] + 20) + "%)";
+
     root.style.setProperty('--secondary-color', secondaryColorHSL);
+    root.style.setProperty('--icon-color', iconColor);
+    root.style.setProperty('--flag-color', flagColor);
     root.style.setProperty('--opposite-color', oppositeColorHSL);
+    root.style.setProperty('--opposite-color-lighter', oppositeColorHSLLighter);
+
 
   })();
-
-
-//   hsl(171, 90%, 28%);
-
---opposite-color
---
+++ src/modules/note_constructer.js
@@ -0,0 +1,12 @@
+//note constructor
+class Book {
+    constructor(title, description, dueDate, priority,) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      this.tags = tags;
+    }
+  }
+
+
