8362e0410210b40804e41ea1f0c6b2639b911814
lucafisc <lucafischer@fastmail.com>
2022-07-07 10:12:34 +0200

    Save title description and date values

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,6 +1,8 @@
 import { pubsub } from "./pubsub.js";
 import { newNote } from "./newNote.js";
 import { doc } from "prettier";
+import { format, parseISO, isToday, isTomorrow } from 'date-fns';
+
 
 export const domControl = () => {
 
@@ -13,12 +15,13 @@ pubsub.subscribe("item-title-click", (data) => {
 
   //save click event
 pubsub.subscribe("save-btn-click", (btn) => {
-    setNote(btn);
+    changeColor(btn);
+    getNoteInput(btn);
  })
 
    //edit click event
 pubsub.subscribe("edit-btn-click", (btn) => {
-    setNote(btn);
+    changeColor(btn);
  })
 
 
@@ -31,7 +34,7 @@ pubsub.subscribe("edit-btn-click", (btn) => {
 
 };
 
-function setNote(btn) {
+function changeColor(btn) {
     let card = btn.parentNode.parentNode.parentNode;
     card.classList.toggle("form");
     card.classList.toggle("todo");
@@ -40,8 +43,43 @@ function setNote(btn) {
     }
 }
 
+function getNoteInput(btn) {
+    let card = btn.parentNode.parentNode.parentNode;
+    let items = getItems(card);
+
+    //title
+    items.itemTitle.textContent = items.inputTitle.textContent;
+    //description
+    items.itemDescription.textContent = items.inputDescription.textContent;
+    //date
+    let newDate = parseISO(items.inputDate.value);
+    if (newDate){
+        items.itemDate.classList.remove("hidden");
+        if (isToday(newDate)){
+            items.itemDate.textContent = "Today";
+        } else if (isTomorrow(newDate)) {
+            items.itemDate.textContent = "Tomorrow";
+        } else {
+        items.itemDate.textContent = format(new Date(newDate), "MMM d");}
+    }
+    else {
+        items.itemDate.classList.add("hidden");
+    }
 
 
+}
+
+
+
+function getItems(card) {
+    let itemTitle = card.querySelector(".item-title");
+    let inputTitle = card.querySelector(".input-title");
+    let itemDescription = card.querySelector(".item-description");
+    let inputDescription = card.querySelector(".input-description");
+    let itemDate = card.querySelector(".item-date");
+    let inputDate = card.querySelector(".input-date");
+    return { itemTitle, inputTitle, itemDescription, inputDescription, itemDate, inputDate };
+}
   //set theme
 //   const themeMenu = document.getElementById("set-theme");
 //   themeMenu.addEventListener("click", () => {
@@ -54,3 +92,7 @@ function setNote(btn) {
 //     let newColor = event.target.style.color;
 //     console.log(newColor);
 //   });
+
+
+// MMM d
+// format(new Date(), "MMM d");
